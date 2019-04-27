6e13aac03bf5b6aca2e018c55fdde4b810794b5c
Zachary Prihoda <zprihoda@stanford.edu>
2019-04-27 11:06:38 -0700

    Only plot setpoint if present
    remove unused t0 variables

--- analysis/plotLogs.py
+++ analysis/plotLogs.py
@@ -18,68 +18,68 @@ TODO: Implement plotMotorRpm once that data is in the logs
 
 def plotPosition(dset_dict):
     local_dset = dset_dict['vehicle_local_position']
-    setpoint_dset = dset_dict['vehicle_local_position_setpoint']
-
-    t0 = local_dset['timestamp'][0]
 
     fig,axes = plt.subplots(3,1,sharex=True)
 
     axes[0].plot((local_dset['timestamp'])/1e6,local_dset['x'])
-    axes[0].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['x'], where='post')
     axes[0].grid()
-    axes[0].legend(['Estimated', 'Setpoint'])
     axes[0].set_ylabel('x (m)')
 
     axes[1].plot((local_dset['timestamp'])/1e6,local_dset['y'])
-    axes[1].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['y'], where='post')
     axes[1].grid()
     axes[1].set_ylabel('y (m)')
 
     axes[2].plot((local_dset['timestamp'])/1e6,local_dset['z'])
-    axes[2].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['z'], where='post')
     axes[2].grid()
     axes[2].set_xlabel('t (s)')
     axes[2].set_ylabel('z (m)')
 
     axes[0].set_title('Position')
 
+    if 'vehicle_local_position_setpoint' in dset_dict:
+        setpoint_dset = dset_dict['vehicle_local_position_setpoint']
+        axes[0].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['x'], where='post')
+        axes[1].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['y'], where='post')
+        axes[2].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['z'], where='post')
+
+    axes[0].legend(['Estimated', 'Setpoint'])
+
     return fig
 
 
 def plotVelocity(dset_dict):
     local_dset = dset_dict['vehicle_local_position']
-    setpoint_dset = dset_dict['vehicle_local_position_setpoint']
-
-    t0 = local_dset['timestamp'][0]
 
     fig,axes = plt.subplots(3,1,sharex=True)
 
     axes[0].plot((local_dset['timestamp'])/1e6,local_dset['vx'])
-    axes[0].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vx'], where='post')
     axes[0].grid()
-    axes[0].legend(['Estimated', 'Setpoint'])
     axes[0].set_ylabel('vx (m/s)')
 
     axes[1].plot((local_dset['timestamp'])/1e6,local_dset['vy'])
-    axes[1].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vy'], where='post')
     axes[1].grid()
     axes[1].set_ylabel('vy (m/s)')
 
     axes[2].plot((local_dset['timestamp'])/1e6,local_dset['vz'])
-    axes[2].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vz'], where='post')
     axes[2].grid()
     axes[2].set_xlabel('t (s)')
     axes[2].set_ylabel('vz (m/s)')
 
     axes[0].set_title('Velocity')
 
+    if 'vehicle_local_position_setpoint' in dset_dict:
+        setpoint_dset = dset_dict['vehicle_local_position_setpoint']
+        axes[0].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vx'], where='post')
+        axes[1].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vy'], where='post')
+        axes[2].step((setpoint_dset['timestamp'])/1e6,setpoint_dset['vz'], where='post')
+
+    axes[0].legend(['Estimated', 'Setpoint'])
+
     return fig
 
 
 def plotAcceleration(dset_dict):
     sensor_dset = dset_dict['sensor_combined']
-
-    t0 = sensor_dset['timestamp'][0]
 
     fig,axes = plt.subplots(3,1,sharex=True)
 
@@ -103,28 +103,26 @@ def plotAcceleration(dset_dict):
 
 
 def plotTrajectory(dset_dict):
-
     local_dset = dset_dict['vehicle_local_position']
-    setpoint_dset = dset_dict['vehicle_local_position_setpoint']
 
     fig,axes = plt.subplots(1,1)
     axes.plot(local_dset['x'],local_dset['y'])
-    axes.plot(setpoint_dset['x'],setpoint_dset['y'])
     axes.grid()
-    axes.legend(['Estimated', 'Setpoint'])
     axes.set_xlabel('x (m)')
     axes.set_ylabel('y (m)')
     axes.set_title('Trajectory')
+
+    if 'vehicle_local_position_setpoint' in dset_dict:
+        setpoint_dset = dset_dict['vehicle_local_position_setpoint']
+        axes.plot(setpoint_dset['x'],setpoint_dset['y'],'.')
+
+    axes.legend(['Estimated', 'Setpoint'])
 
     return fig
 
 
 def plotAttitude(dset_dict):
-
     attitude_dset = dset_dict['vehicle_attitude']
-    setpoint_dset = dset_dict['vehicle_attitude_setpoint']
-
-    t0 = attitude_dset['timestamp'][0]
 
     fig,axes = plt.subplots(3,1,sharex=True)
 
@@ -136,66 +134,71 @@ def plotAttitude(dset_dict):
     pitch = np.arcsin(2.0 *(q_0 * q_2 - q_3 * q_1))
     yaw = np.arctan2(2.0 * (q_0 * q_3 + q_1 * q_2), 1.0 - 2.0 * (q_2 * q_2 + q_3 * q_3))
 
-    q_0 = setpoint_dset['q_d[0]']
-    q_1 = setpoint_dset['q_d[1]']
-    q_2 = setpoint_dset['q_d[2]']
-    q_3 = setpoint_dset['q_d[3]']
-    roll_set = np.arctan2(2.0 * (q_0 * q_1 + q_2 * q_3), 1.0 - 2.0 * (q_1 * q_1 + q_2 * q_2))
-    pitch_set = np.arcsin(2.0 * (q_0 * q_2 - q_3 * q_1))
-    yaw_set = np.arctan2(2.0 * (q_0 * q_3 + q_1 * q_2), 1.0 - 2.0 * (q_2 * q_2 + q_3 * q_3))
-
     axes[0].plot((attitude_dset['timestamp'])/1e6,roll)
-    axes[0].plot((setpoint_dset['timestamp'])/1e6,roll_set)
     axes[0].grid()
-    axes[0].legend(['Estimated', 'Setpoint'])
     axes[0].set_ylabel('roll (rad)')
 
     axes[1].plot((attitude_dset['timestamp'])/1e6,pitch)
-    axes[1].plot((setpoint_dset['timestamp'])/1e6,pitch_set)
     axes[1].grid()
     axes[1].set_ylabel('pitch (rad)')
 
     axes[2].plot((attitude_dset['timestamp'])/1e6,yaw)
-    axes[2].plot((setpoint_dset['timestamp'])/1e6,yaw_set)
     axes[2].grid()
     axes[2].set_xlabel('t (s)')
     axes[2].set_ylabel('yaw (rad)')
 
     axes[0].set_title('Attitude')
 
+    if 'vehicle_attitude_setpoint' in dset_dict:
+        setpoint_dset = dset_dict['vehicle_attitude_setpoint']
+        q_0 = setpoint_dset['q_d[0]']
+        q_1 = setpoint_dset['q_d[1]']
+        q_2 = setpoint_dset['q_d[2]']
+        q_3 = setpoint_dset['q_d[3]']
+        roll_set = np.arctan2(2.0 * (q_0 * q_1 + q_2 * q_3), 1.0 - 2.0 * (q_1 * q_1 + q_2 * q_2))
+        pitch_set = np.arcsin(2.0 * (q_0 * q_2 - q_3 * q_1))
+        yaw_set = np.arctan2(2.0 * (q_0 * q_3 + q_1 * q_2), 1.0 - 2.0 * (q_2 * q_2 + q_3 * q_3))
+
+        axes[0].plot((setpoint_dset['timestamp'])/1e6,roll_set)
+        axes[1].plot((setpoint_dset['timestamp'])/1e6,pitch_set)
+        axes[2].plot((setpoint_dset['timestamp'])/1e6,yaw_set)
+
+    axes[0].legend(['Estimated', 'Setpoint'])
+
     return fig
 
 
 def plotAttitudeRates(dset_dict):
-
     rates_dset = dset_dict['vehicle_attitude']
-    setpoint_dset = dset_dict['vehicle_rates_setpoint']
 
     t0 = rates_dset['timestamp'][0]
     tf = rates_dset['timestamp'][-1]
 
-    idx = np.logical_and(setpoint_dset['timestamp'] > t0, setpoint_dset['timestamp'] < tf)
-
     fig,axes = plt.subplots(3,1,sharex=True)
 
     axes[0].plot((rates_dset['timestamp'])/1e6,rates_dset['rollspeed'])
-    axes[0].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['roll'][idx])
     axes[0].grid()
-    axes[0].legend(['Estimated', 'Setpoint'])
     axes[0].set_ylabel('roll rates (rad/s)')
 
     axes[1].plot((rates_dset['timestamp'])/1e6,rates_dset['pitchspeed'])
-    axes[1].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['pitch'][idx])
     axes[1].grid()
     axes[1].set_ylabel('pitch rates (rad/s)')
 
     axes[2].plot((rates_dset['timestamp'])/1e6,rates_dset['yawspeed'])
-    axes[2].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['yaw'][idx])
     axes[2].grid()
     axes[2].set_xlabel('t (s)')
     axes[2].set_ylabel('yaw rates (rad/s)')
 
     axes[0].set_title('Attitude Rates')
+
+    if 'vehicle_rates_setpoint' in dset_dict:
+        setpoint_dset = dset_dict['vehicle_rates_setpoint']
+        idx = np.logical_and(setpoint_dset['timestamp'] > t0, setpoint_dset['timestamp'] < tf)
+        axes[0].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['roll'][idx])
+        axes[1].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['pitch'][idx])
+        axes[2].plot((setpoint_dset['timestamp'][idx])/1e6,setpoint_dset['yaw'][idx])
+
+    axes[0].legend(['Estimated', 'Setpoint'])
 
     return fig
 
@@ -204,8 +207,6 @@ def plotBatteryStatus(dset_dict):
     # plot voltage and current
 
     battery_set = dset_dict['battery_status']
-
-    t0 = battery_set['timestamp'][0]
 
     fig,axes = plt.subplots(1,1)
     axes.plot((battery_set['timestamp'])/1e6,battery_set['current_a'])
@@ -225,8 +226,6 @@ def plotActuator(dset_dict):
     # plot actuator control
 
     actuator_set = dset_dict['actuator_outputs']
-
-    t0 = actuator_set['timestamp'][0]
 
     fig,axes = plt.subplots(1,1)
     axes.plot((actuator_set['timestamp'])/1e6,actuator_set['output[0]'])
